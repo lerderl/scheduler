f94b48fa74c66393d66e4d3f978c7b70fe590d5c
Joseph Olukunle <josepholukunle1107@gmail.com>
2025-04-05 19:39:08 +0100

    Data in Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,15 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.println("Hello Joseph");
+
+//    Variables, Constants and Data Types ( Data in Java )
+    int age = 19;
+    final int SOME_NUMBER = 20; // Symbolic constant
+    String name = "Joseph";
+    String hometown = "Lekki";
+    System.out.println(name + " is " + age + " years old");
+    System.out.print(name + " is " + age + " years old\n");
+    System.out.println("My hometown is " + hometown);
+    System.out.print("Constant is " + SOME_NUMBER);
   }
 }
